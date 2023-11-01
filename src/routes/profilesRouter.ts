483d223d24ef7a53eec289d2c3c2d5cad4f5ba91
Echoesong <jayefelker@gmail.com>
2023-11-01 17:04:23 -0500

    mount checkJwt service to routes

--- src/routes/profilesRouter.ts
+++ src/routes/profilesRouter.ts
@@ -1,10 +1,11 @@
 import { profilesController } from "../controllers/profilesController";
+import checkJwt from "../services/checkJwt";
 import express from "express";
 const router = express.Router();
 
 // CREATE
 router.post("/", profilesController.create);
 // SHOW/DETAILS
-router.get("/:email", profilesController.getOne);
+router.get("/:email", checkJwt, profilesController.getOne);
 
 export default router;
