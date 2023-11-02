0ab0e7bbff0bd9e90c4b858c81577d917b734c3d
mgballou <matthewbuiltthat@gmail.com>
2023-11-02 10:02:08 -0400

    heroku hotfix 2

--- src/prestart.ts
+++ src/prestart.ts
@@ -32,9 +32,7 @@ const args = parse<IArgs>({
 
 
 // Set the env file
-const result2 = dotenv.config({
-  path: path.join(__dirname, `.env`),
-});
+const result2 = dotenv.config();
 if (result2.error) {
   throw result2.error;
 }
