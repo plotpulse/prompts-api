d131aee54ef5f11d4ce2b681c3049216aacce208
mgballou <matthewbuiltthat@gmail.com>
2023-11-02 10:15:06 -0400

    heroku fix again

--- src/prestart.ts
+++ src/prestart.ts
@@ -1,38 +1,11 @@
-/**
- * Pre-start is where we want to place things that must run BEFORE the express 
- * server is started. This is useful for environment variables, command-line 
- * arguments, and cron-jobs.
- */
-
-// NOTE: DO NOT IMPORT ANY SOURCE CODE HERE
 import path from 'path';
 import dotenv from 'dotenv';
-import { parse } from 'ts-command-line-args';
 
-
-// **** Types **** //
-
-interface IArgs {
-  env: string;
-}
-
-
-// **** Setup **** //
-
-// Command line arguments
-const args = parse<IArgs>({
-  env: {
-    type: String,
-    defaultValue: 'development',
-    alias: 'e',
-    optional: true
-  },
+// Set the env file (assuming it's in the root directory)
+const result = dotenv.config({
+  path: path.join(__dirname, '../.env'),
 });
 
-
-
-// Set the env file
-const result2 = dotenv.config();
-if (result2.error) {
-  throw result2.error;
+if (result.error) {
+  throw result.error;
 }
