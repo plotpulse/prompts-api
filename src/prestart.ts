854a59e4c472349362899c14dc1b4d0fb8cc3b9f
mgballou <matthewbuiltthat@gmail.com>
2023-11-02 10:20:30 -0400

    fixes not working, rollback to working local version

--- src/prestart.ts
+++ src/prestart.ts
@@ -1,9 +1,38 @@
+/**
+ * Pre-start is where we want to place things that must run BEFORE the express 
+ * server is started. This is useful for environment variables, command-line 
+ * arguments, and cron-jobs.
+ */
+
+// NOTE: DO NOT IMPORT ANY SOURCE CODE HERE
 import path from 'path';
 import dotenv from 'dotenv';
+import { parse } from 'ts-command-line-args';
 
-// Set the env file (assuming it's in the root directory)
-const result = dotenv.config();
 
-if (result.error) {
-  throw result.error;
+// **** Types **** //
+
+interface IArgs {
+  env: string;
+}
+
+
+// **** Setup **** //
+
+// Command line arguments
+const args = parse<IArgs>({
+  env: {
+    type: String,
+    defaultValue: 'development',
+    alias: 'e',
+    optional: true
+  },
+});
+
+// Set the env file
+const result2 = dotenv.config({
+  path: path.join(__dirname, `../src/env/${args.env}.env`),
+});
+if (result2.error) {
+  throw result2.error;
 }
