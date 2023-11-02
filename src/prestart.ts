bcaa742e834388e7bceecb9d89f0949401a78981
mgballou <matthewbuiltthat@gmail.com>
2023-11-02 10:18:56 -0400

    heroku 5

--- src/prestart.ts
+++ src/prestart.ts
@@ -2,9 +2,7 @@ import path from 'path';
 import dotenv from 'dotenv';
 
 // Set the env file (assuming it's in the root directory)
-const result = dotenv.config({
-  path: path.join(__dirname, '../.env'),
-});
+const result = dotenv.config();
 
 if (result.error) {
   throw result.error;
